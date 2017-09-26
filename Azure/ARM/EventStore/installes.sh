a7b0ccbef9bf6a39fe89eeac34685370b117f452
Joshua Smith <joshua.smith@amgh.us>
2017-09-26 13:13:36 -0500

    cleaned up install scripts

--- Azure/ARM/EventStore/installes.sh
+++ Azure/ARM/EventStore/installes.sh
@@ -1,6 +1,8 @@
+#!/bin/bash
+
 sudo apt-get update
 sudo apt-get install vim
-curl -s https://packagecloud.io/install/repositories/EventStore/EventStore-OSS/script.deb.sh.deb | sudo bash
+curl -s https://packagecloud.io/install/repositories/EventStore/EventStore-OSS/script.deb.sh | sudo bash
 sudo apt-get install eventstore-oss=4.0.0
 sudo mkdir -p /data1/esdb
 sudo mkdir -p /data1/esindex

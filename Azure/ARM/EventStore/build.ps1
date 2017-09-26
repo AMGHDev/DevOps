e368e254a99c29b95bdae29d18bbf64b53e86033
Joshua Smith <joshua.smith@amgh.us>
2017-09-26 12:32:52 -0500

    modified vmss setup scripts; corrections from testing script

--- Azure/ARM/EventStore/build.ps1
+++ Azure/ARM/EventStore/build.ps1
@@ -1,20 +1,20 @@
 
 # log into the subscription
-$subscriptionId = "your-subscription-id"
-$location = "Azure Location Name"
+$subscriptionId = "your-sub-id"
+$location = "azure location"
 Login-AzureRmAccount -SubscriptionId $subscriptionId
 
 $masterPrefix = "prfx"
-$randomMix = "XJDL"
+$randomMix = "R8X3S"
 $eventStoreAdminUsername = "admin"
 $jumpBoxAdminUsername = "admin"
-$eventStoreAdminPassword = ConvertTo-SecureString -String "" -AsPlainText -Force
-$eventStoreJumpBoxAdminPassword = ConvertTo-SecureString -String "" -AsPlainText -Force
-$certificatePassword = "cert-pw"
+$eventStoreAdminPassword = ConvertTo-SecureString -String "todo" -AsPlainText -Force
+$eventStoreJumpBoxAdminPassword = ConvertTo-SecureString -String "todo" -AsPlainText -Force
+$certificatePassword = ""
 $certificateContent = [System.Convert]::ToBase64String([System.IO.File]::ReadAllBytes("c:\path\to\x.pfx"))
 
 # create the resource group for ES
-$eventStoreResourceGroupName = "$masterPrefix-$randomMix-rg"
+$eventStoreResourceGroupName = "$masterPrefix-$randomMix-resource-group"
 New-AzureRmResourceGroup -Name $eventStoreResourceGroupName  -Location $location 
 
 # build out the ES resources
@@ -23,4 +23,4 @@ New-AzureRmResourceGroupDeployment -Name "$masterPrefix-$randomMix-esdeploy-01" 
             -TemplateFile ".\event-store.json" -TemplateParameterFile ".\event-store.parameters.json" `
             -eventStoreAdminUsername $eventStoreAdminUsername -eventStoreAdminPassword $eventStoreAdminPassword `
             -jumpBoxAdminUsername $jumpBoxAdminUsername -jumpBoxAdminPassword $eventStoreJumpBoxAdminPassword -randomMix $randomMix `
-            -certificatePassword $certificatePassword -certificateBase64EncodedValue $certificateContent -masterPrefix $masterPrefix -randomMix $randomMix
+            -certificatePassword $certificatePassword -certificateBase64EncodedValue $certificateContent -masterPrefix $masterPrefix

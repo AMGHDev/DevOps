777493a9af29391a174dfda4343c2d4207a1d8ff
Joshua Smith <joshua.smith@amgh.us>
2017-09-26 22:08:29 -0500

    slight adjustment

--- Azure/ARM/EventStore/installes.sh
+++ Azure/ARM/EventStore/installes.sh
@@ -6,7 +6,8 @@ then
     echo "Event store already installed"
 else
     sudo apt-get update
-    sudo apt-get install vim
+    sudo apt-get install vim --yes
+    sudo apt-get install monodevel --yes
     curl -s https://packagecloud.io/install/repositories/EventStore/EventStore-OSS/script.deb.sh | sudo bash
     sudo apt-get install eventstore-oss=4.0.0
     sudo mkdir -p /data1/esdb

5e6fab9bb97c962d0f42ecef60cada3638ee2a2f
Joshua Smith <joshua.smith@amgh.us>
2017-09-27 20:47:45 -0500

    started config

--- Azure/ARM/EventStore/installes.sh
+++ Azure/ARM/EventStore/installes.sh
@@ -13,8 +13,31 @@ else
     sudo mkdir -p /data1/esdb
     sudo mkdir -p /data1/esindex
     sudo mkdir -p /data1/eslogs
+	sudo mkdir -p /data1/temp
     sudo chown eventstore:eventstore /data1/esdb
     sudo chown eventstore:eventstore /data1/esindex
+	sudo chown eventstore:eventstore /data1/eslogs
+	sudo chown eventstore:eventstore /data1/temp
     cd /etc/eventstore/
     sudo rm eventstore.conf
+	
+	sudo sh -c 'cat << EOF >> eventstore.conf
+IntIp: 10.0.2.5
+ExtIp: 0.0.0.0
+
+IntHttpPrefixes: http://10.0.2.5:2112/
+ExtHttpPrefixes: http://*:2113/
+
+Db: /data1/esdb
+Index: /data1/esindex
+Log: /data1/eslogs
+
+AddInterfacePrefixes: false
+RunProjections: All
+ClusterSize: 2
+DiscoverViaDns: False
+
+GossipSeed: 10.0.3.5:2112,10.0.3.6:2112,10.0.3.7:2112,10.0.3.8:2112
+EOF'
+
 fi
